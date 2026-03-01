5a251f682ba34d453a2062b15157b200e1270fe5
mdsamiulislam54 <samiulm5332@gmail.com>
2026-03-01 10:54:23 +0600

    Zod validation

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,9 +2,9 @@
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 import { IErrorSource, zodError } from "./zodError";
+import { envConfig } from "../../config/envConfig";
 
 export const globalErrorHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
-
     let statusCode = 500;
     let message = err.message || "An unexpected error occurred";
     let errorSource: IErrorSource[] = [];
@@ -18,8 +18,9 @@ export const globalErrorHandler = (err: Error, req: Request, res: Response, _nex
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message: message,
-        errorSource
+        errorSource,
+        message: envConfig.NODE_ENV === 'development' ? message : undefined,
+
     });
 
 }
